24214b3352e9f17ca50248c6c94dfee4fdaf83d8
thanhdc97 <thanh.dc@haposoft.com>
2021-07-25 15:47:50 +0700

    fix_convention

--- js/main.js
+++ js/main.js
@@ -1,34 +1,32 @@
 $(document).ready(function() {
-    $('.feed-slide').slick({
-        infinite: true,
-        slidesToShow: 2,
-        slidesToScroll: 2,
-        // autoplay: true,
-        prevArrow:"<div class='arrow-left'><i class='fas fa-angle-left'></i></div>",
-        nextArrow:"<div class='arrow-right'><i class='fas fa-angle-right'></i></div>",
-        responsive: [
-            {
-              breakpoint: 768,
-              settings: {
-                slidesToShow: 1,
-                slidesToScroll: 1,
-              }
-            }
-        ]
-    });
+  $('.feed-slide').slick({
+    infinite: true,
+    slidesToShow: 2,
+    slidesToScroll: 2,
+    autoplay: true,
+    prevArrow:"<div class='arrow-left'><i class='fas fa-angle-left'></i></div>",
+    nextArrow:"<div class='arrow-right'><i class='fas fa-angle-right'></i></div>",
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          }
+      }
+    ]
+  });
 
-    $('.counter').countUp();
+  $('.counter').countUp();
 
-    $('.social-link .icon').click(function() {
-        var item = $(this).parent();
-        if ($(item).hasClass('active')) {
-            $(item).removeClass('active');
-        } else {
-            var socialLink = $(item).parents('.social-link');
-            socialLink.find('li .item').removeClass('active');
-            $(item).addClass('active');
-        }
-        
-    })
-
+  $('.social-link .icon').click(function() {
+    var item = $(this).parent();
+    if ($(item).hasClass('active')) {
+        $(item).removeClass('active');
+    } else {
+        var socialLink = $(item).parents('.social-link');
+        socialLink.find('li .item').removeClass('active');
+        $(item).addClass('active');
+    }
+  })
 })
